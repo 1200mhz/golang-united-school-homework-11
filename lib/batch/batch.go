0b09d5fee81ece6587a5ce309f44c512c3563c49
Eldar Azimov <chaevnicher@gmail.com>
2022-07-06 12:47:56 +0300

    HW-011: getBatch with errgroup

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -1,9 +1,17 @@
 package batch
 
 import (
+	"context"
 	"sync"
 	"time"
+
+	"golang.org/x/sync/errgroup"
 )
+
+type users struct {
+	items []user
+	mu    sync.Mutex
+}
 
 type user struct {
 	ID int64
@@ -15,11 +23,31 @@ func getOne(id int64) user {
 }
 
 func getBatch(n int64, pool int64) (res []user) {
-	type users struct {
-		items []user
-		mu    sync.Mutex
+	result := &users{}
+
+	eg, _ := errgroup.WithContext(context.Background())
+	eg.SetLimit(int(pool))
+
+	var i int64
+	for i = 0; i < n; i++ {
+		func(i int64) {
+			eg.Go(func() error {
+				u := getOne(i)
+				result.mu.Lock()
+				result.items = append(result.items, u)
+				result.mu.Unlock()
+
+				return nil
+			})
+		}(i)
 	}
 
+	eg.Wait()
+
+	return result.items
+}
+
+func getBatch2(n int64, pool int64) (res []user) {
 	result := &users{}
 	var waitGroup sync.WaitGroup
 	ch := make(chan struct{}, pool)
